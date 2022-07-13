a66b6348b75c487b100921ade87b1d8886fed38f
tokayer <tokayer@users.noreply.github.com>
2022-07-13 10:56:06 +0000

    deploy: 03b3dfb1db87baf4387dfa71f6e796d67cd57453

---
+++ precache-manifest.0df686535e19804724a70848b04ff971.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6fb962c5c742e51ff43ab81fe5cba7",
+    "url": "https://tokayer.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "02dd33f27d51a10d18d9",
+    "url": "https://tokayer.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "721bbcf9c49688c561d1",
+    "url": "https://tokayer.github.io/task-management-frontend/static/js/2.959cb35e.chunk.js"
+  },
+  {
+    "revision": "02dd33f27d51a10d18d9",
+    "url": "https://tokayer.github.io/task-management-frontend/static/js/main.561c973e.chunk.js"
+  },
+  {
+    "revision": "3607c23a7bc60e64bb44",
+    "url": "https://tokayer.github.io/task-management-frontend/static/js/runtime~main.036e4002.js"
+  }
+]);
